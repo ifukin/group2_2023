87ff984414f6599212dac12f963d1d2e41d15cb1
ifukin <57060355+ifukin@users.noreply.github.com>
2023-11-17 17:18:31 +0300

    Кто-то сделал Update main.py
    шгп фыжрп ждраип

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print("Внесли еще один принт")
+    print("Кто-то пришел и написал эту строку, возможно я")
 
 
 # Press the green button in the gutter to run the script.
